cd8e4ea1833bf5ded8646a2ca8ddef83e37e7334
= <123371615+AjeerAkratech@users.noreply.github.com>
2023-07-02 02:05:05 +0530

    feat: register ui

--- components/Auth/Register.tsx
+++ components/Auth/Register.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Button, Text, TextInput, TouchableOpacity } from "react-native";
+import { View } from "../Themed";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <SafeAreaView>
+      <View className="h-full flex p-5 justify-center ">
+        <View>
+          <Text className="font-bold text-4xl  text-main-default">
+            Register
+          </Text>
+        </View>
+        <View>
+          <View>
+            <View className="my-2">
+              <Text className="m-1 text-gray-600">Email Adress</Text>
+              <TextInput
+                className={"py-2 px-4 border border-gray-300 rounded-md"}
+                placeholder="Enter your email..."
+              />
+            </View>
+            <View>
+              <TouchableOpacity className="bg-main-default rounded ">
+                <Button color="white" title="Continue" />
+              </TouchableOpacity>
+            </View>
+          </View>
 
-export default Register
+          <Text className="text-center text-gray-400 my-5 ">Or</Text>
+
+          <View className="my-2">
+            <TouchableOpacity className="bg-slate-100 rounded ">
+              <Button color="gray" title="Sign in with Google" />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity className="bg-slate-100 rounded ">
+              <Button color="gray" title="Sign in with Apple" />
+            </TouchableOpacity>
+          </View>
+          <Text className="font-medium text-gray-500 text-center my-2">
+            Already have an account?
+            <Text className="text-main-default font-bold"> Login</Text>
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Register;
