d05a221114e38d783a7475c85c1bf254cadfecf9
= <123371615+AjeerAkratech@users.noreply.github.com>
2023-07-05 00:09:10 +0530

    fix: router refactor

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Register from "../components/Auth/Register";
+import { Stack } from "expo-router";
+
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "index",
+};
+
+const Index = () => {
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <Register />
+    </>
+  );
+};
+
+export default Index;
